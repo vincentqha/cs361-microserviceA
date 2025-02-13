0fc28f28f4b8e5c19cf66f3e63cf5779ae72998b
Vincent Ha <vincentha@g.ucla.edu>
2025-02-13 14:46:55 -0800

    Add API call

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Test message for Microservice A!")
+import requests
+from pprint import pprint
+
+API_URL = "https://quoteapi.pythonanywhere.com/quotes/"
+
+print(f"\nTo query a movie quote, please enter a movie title.\n")
+
+# user_input = input("Please enter a movie title: ")
+
+response = requests.get(API_URL)
+
+if response.status_code != 200:
+    print("Error: Unable to retrieve movie quotes")
+
+quotes_data = response.json()
+pprint(quotes_data)
+
