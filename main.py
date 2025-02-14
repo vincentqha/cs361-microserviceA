23cdc0f680c007a210c23cecfcbc0f4037ed7a03
Vincent Ha <vincentha@g.ucla.edu>
2025-02-14 10:37:54 -0800

    Implement movie searching logic and quote output

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ from pprint import pprint
 
 API_URL = "https://quoteapi.pythonanywhere.com/quotes/"
 
-print(f"\nTo query a movie quote, please enter a movie title.\n")
-
-# user_input = input("Please enter a movie title: ")
-
 response = requests.get(API_URL)
 
 if response.status_code != 200:
     print("Error: Unable to retrieve movie quotes")
 
 quotes_data = response.json()
-pprint(quotes_data["Quotes"][0][0]["movie_title"])
 
 # quotes_data["Quotes"][0][object_num][property]
 # print(len(quotes_data["Quotes"][0]))
 # print(quotes_data["Quotes"][0][81])
 
 movies = []
+quotes = quotes_data["Quotes"][0]
 
 for num in range(len(quotes_data["Quotes"][0])):
-    movies.append(quotes_data["Quotes"][0][num]["movie_title"])
+    movies.append(quotes[num]["movie_title"])
 
+print(f"\nTo query a movie quote, please enter a movie title.\n")
+
+user_input = input("Please enter a movie title: ")
+
+if user_input in movies:
+    movie_index = movies.index(user_input)
+    print(f"The quote from {movies[movie_index]} is: \"{quotes[movie_index]["quote"]}\"")
+else:
+    print("The movie you entered does not exist in our database. Please enter a different movie title.")
